8d040fbd69ddad37b91e37358f5b5c4ffbaab7b6
Adrien Mazoyer <adrien.mazoyer@univ-grenoble-alpes.fr>
2017-06-01 09:58:14 +0200

    Typo

--- ui.R
+++ ui.R
@@ -26,7 +26,7 @@ shinyUI(fluidPage(
       ),
       fluidRow(
 	column(12,
-	  conditionalPanel(condition = "input.twosample", 
+	  conditionalPanel(condition = "input.twosample",
 	    fileInput(inputId = "sample2", label = "Choose File for 2nd sample",
 	      accept = c(
 		"text/csv",
@@ -44,7 +44,7 @@ shinyUI(fluidPage(
 	  checkboxInput(inputId = "estfit", label = "Unknown fitness", value = TRUE)
 	),
 	column(6,
-	  selectInput(inputId = "model", label = "Growth model", 
+	  selectInput(inputId = "model", label = "Growth model",
 	    choices = c("LD" = "LD", "H" = "H"),
 	    selected = "LD"
 	  )
@@ -52,14 +52,14 @@ shinyUI(fluidPage(
       ),
       fluidRow(
 	column(6,
-	  selectInput(inputId = "method", label = "Estimation Method", 
+	  selectInput(inputId = "method", label = "Estimation Method",
 	    choices = c("ML" = "ML", "GF" = "GF", "P0" = "P0"),
 	    selected = "ML"
 	  )
 	),
 	conditionalPanel(condition = "input.method == 'ML' || (input.method == 'P0' && input.estfit)",
 	  column(6,
-	    numericInput(inputId = "winsor", "Winsor parameter", 
+	    numericInput(inputId = "winsor", "Winsor parameter",
 	      value = 1024, min = 0
 	    )
 	  )
@@ -75,22 +75,22 @@ shinyUI(fluidPage(
       fluidRow(
 	column(6,
 	  conditionalPanel(condition = "input.twosample && (input.mfn1 < 0)",
-	    numericInput(inputId = "mutations0", "Mutations number difference", 
+	    numericInput(inputId = "mutations0", "Mutations number difference",
 	      value = 0, step = 0.1
 	    )
 	  ),
 	  conditionalPanel(condition = "!input.twosample && (input.mfn1 < 0)",
-	    numericInput(inputId = "mutations0", "Mutations number", 
+	    numericInput(inputId = "mutations0", "Mutations number",
 	      value = 1, min = 0, step = 0.1
 	    )
 	  ),
 	  conditionalPanel(condition = "input.twosample && (input.mfn1 > 0)",
-	    numericInput(inputId = "mutprob0", "Mutation probability difference", 
+	    numericInput(inputId = "mutprob0", "Mutation probability difference",
 	      value = 0, step = 1e-10
 	    )
 	  ),
 	  conditionalPanel(condition = "!input.twosample && (input.mfn1 > 0)",
-	    numericInput(inputId = "mutations0", "Mutation probability", 
+	    numericInput(inputId = "mutations0", "Mutation probability",
 	      value = 1, min = 0, step = 1e-10
 	    )
 	  )
@@ -98,12 +98,12 @@ shinyUI(fluidPage(
 	conditionalPanel(condition = "input.estfit",
 	  column(6,
 	    conditionalPanel(condition = "input.twosample",
-	      numericInput(inputId = "fitness0", "Fitness difference", 
+	      numericInput(inputId = "fitness0", "Fitness difference",
 		value = 0, step = 0.1
 	      )
 	    ),
 	    conditionalPanel(condition = "!input.twosample",
-	      numericInput(inputId = "fitness0", "Fitness", 
+	      numericInput(inputId = "fitness0", "Fitness",
 		value = 1, min = 0, step = 0.1
 	      )
 	    )
@@ -133,35 +133,35 @@ shinyUI(fluidPage(
 	  )
 	)
       ),
-      conditionalPanel(condition = "input.estfit",
-	tags$h4(tags$strong("Confidence levels"))
-      ),
-      conditionalPanel(condition = "!input.estfit",
-	tags$h4(tags$strong("Confidence level"))
-      ),
+  #     conditionalPanel(condition = "input.estfit",
+	# tags$h4(tags$strong("Confidence levels"))
+  #     ),
+  #     conditionalPanel(condition = "!input.estfit",
+	# tags$h4(tags$strong("Confidence level"))
+  #     ),
       fluidRow(
 	column(6,
-	  conditionalPanel(condition = "input.mfn1 > 0",
-	    numericInput(inputId = "mutconf", label = "Mutation probability",
+	  # conditionalPanel(condition = "input.mfn1 > 0",
+	    numericInput(inputId = "conflevel", label = "Confidence level",
 	      value = 0.95, min = 0, max = 1, step = 0.01
 	    )
-	  ),
-	  conditionalPanel(condition = "input.mfn1 < 0",
-	    numericInput(inputId = "mutconf", label = "Mutations number",
-	      value = 0.95, min = 0, max = 1, step = 0.01
-	    )
-	  )
-	),
-	conditionalPanel(condition = "input.estfit",
-	  column(6,
-	    numericInput(inputId = "fitconf", label = "Fitness",
-	      value = 0.95, min = 0, max = 1, step = 0.01
-	    )
-	  )
+	  # ),
+	  # conditionalPanel(condition = "input.mfn1 < 0",
+	  #   numericInput(inputId = "mutconf", label = "Mutations number",
+	  #     value = 0.95, min = 0, max = 1, step = 0.01
+	  #   )
+	  # )
 	)
+	# conditionalPanel(condition = "input.estfit",
+	#   column(6,
+	#     numericInput(inputId = "fitconf", label = "Fitness",
+	#       value = 0.95, min = 0, max = 1, step = 0.01
+	#     )
+	#   )
+	# )
       )
     ),
-    mainPanel( 
+    mainPanel(
       tags$h4(tags$strong("Sample 1")),
       fluidRow(
 	column(12,
@@ -181,18 +181,18 @@ shinyUI(fluidPage(
   	  )
   	),
 	column(2,
-	  numericInput(inputId = "mfn1", "Mean Final Number", 
+	  numericInput(inputId = "mfn1", "Mean Final Number",
 	    value = -1, step = 0.1
 	  )
 	),
 	column(3,
-	  numericInput(inputId = "cvfn1", "Coef. Variation Final Number", 
+	  numericInput(inputId = "cvfn1", "Coef. Variation Final Number",
 	    value = -1
 	  )
   	),
 	conditionalPanel(condition = "!(input.estfit)",
 	  column(2,
-	    numericInput(inputId = "fitvalue1", "Fitness Value", 
+	    numericInput(inputId = "fitvalue1", "Fitness Value",
 	      value = 1, min = 0, max = 10
 	    )
 	  )
@@ -218,18 +218,18 @@ shinyUI(fluidPage(
 	    )
 	  ),
 	  column(2,
-	    numericInput(inputId = "mfn2", "Mean Final Number", 
+	    numericInput(inputId = "mfn2", "Mean Final Number",
 	      value = -1
 	    )
 	  ),
 	  column(3,
-	    numericInput(inputId = "cvfn2", "Coef. Variation Final Number", 
+	    numericInput(inputId = "cvfn2", "Coef. Variation Final Number",
 	      value = -1
 	    )
 	  ),
 	  conditionalPanel(condition = "!(input.estfit)",
 	    column(2,
-	      numericInput(inputId = "fitvalue2", "Fitness Value", 
+	      numericInput(inputId = "fitvalue2", "Fitness Value",
 		value = 1, min = 0, max = 10
 	      )
 	    )
@@ -237,7 +237,7 @@ shinyUI(fluidPage(
 	)
       ),
       actionButton(inputId = "launch", label = "Launch"),
-  #       
+  #
   #   ),
   #     ),
   # Print flan.test result
